3f4fa214de8c1572c14230472d9d61fafc8d0606
tigeet <tigeeet@gmail.com>
2023-05-03 12:33:03 +0300

    set up initial tsx files

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import App from "./App.tsx";
 import "@/reset.scss";
 import "@/global.scss";
 import "@/colors.scss";
+
+import App from "./app.tsx";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
