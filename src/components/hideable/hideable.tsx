eacd9c3d8648b85b86c9ba6c3d13c668dad3e933
tigeet <tigeeet@gmail.com>
2023-05-11 20:27:49 +0300

    Hideable: stable version

---
+++ src/components/hideable/hideable.tsx
@@ -0,0 +1,80 @@
+import React, { useEffect, useRef } from "react";
+import "./hideable.scss";
+import { Eye, EyeOff } from "react-feather";
+
+interface HideableProps {
+  theme?: "light" | "dark";
+  value: string;
+  visible: boolean;
+  focused?: boolean;
+  placeholder?: string;
+  onValueChange?: (value: string) => void;
+  toggleVisible?: () => void;
+  toggleFocused?: () => void;
+}
+
+const Hideable = ({
+  theme = "dark",
+  value,
+  visible,
+  focused = false,
+  placeholder = "",
+  onValueChange,
+  toggleVisible,
+  toggleFocused,
+}: HideableProps) => {
+  const handleBlur = (e: React.FocusEvent) => {
+    if (e.currentTarget.contains(e.relatedTarget)) return;
+    focused && toggleFocused?.();
+  };
+  const handleFocus = (e) => {
+    !focused && toggleFocused?.();
+  };
+  const displayVisibilitySwitch = toggleVisible !== undefined;
+  return (
+    <div tabIndex={0} className="hideable" data-theme={theme}>
+      <div className="hideable__input-wrapper">
+        <input
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          type={visible ? "text" : "password"}
+          className="hideable__input cursor-pointer"
+          value={value}
+          placeholder={placeholder}
+          readOnly={!focused}
+          onChange={(e) => onValueChange?.(e.target.value)}
+        />
+        <div
+          className={[
+            "hideable__underline",
+            `hideable__underline-${focused ? "visible" : "hidden"}`,
+          ].join(" ")}
+        />
+      </div>
+      {displayVisibilitySwitch && (
+        <div
+          className="hideable__visibility-switch centered"
+          onClick={toggleVisible}
+        >
+          {visible ? (
+            <Eye
+              size={16}
+              color={
+                theme == "light" ? "rgb(125, 129, 136)" : "rgb(154, 160, 166)"
+              }
+            />
+          ) : (
+            <EyeOff
+              size={16}
+              color={
+                theme == "light" ? "rgb(125, 129, 136)" : "rgb(154, 160, 166)"
+              }
+            />
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Hideable;
---
+++ src/components/hideable/story/hideable.stories.tsx
@@ -0,0 +1,143 @@
+import { Meta, StoryObj } from "@storybook/react";
+import Hideable from "../hideable";
+import React, { useRef, useState } from "react";
+
+const meta = {
+  title: "Hideable",
+  component: Hideable,
+  argTypes: {
+    theme: {
+      options: ["light", "dark"],
+      control: { type: "select" },
+    },
+  },
+} satisfies Meta<typeof Hideable>;
+export default meta;
+type Story = StoryObj<typeof meta>;
+type Args = Story["args"];
+const Visible = {
+  args: {
+    value: "qwerty1234",
+    visible: true,
+    theme: "light",
+    toggleVisible: () => {
+      null;
+    },
+  },
+} satisfies Story;
+
+const Hidden = {
+  args: {
+    value: "password1234",
+    visible: false,
+    theme: "light",
+    toggleVisible: () => {
+      null;
+    },
+  },
+} satisfies Story;
+
+const WithVisibilitySwitch = {
+  args: {
+    value: "qwerty1234",
+    visible: true,
+    theme: "light",
+    toggleVisible: () => {
+      null;
+    },
+  },
+} satisfies Story;
+
+const WithoutVisibilitySwitch = {
+  args: {
+    value: "qwerty1234",
+    visible: true,
+    theme: "light",
+  },
+} satisfies Story;
+
+const Light = {
+  args: {
+    value: "password1234",
+    visible: true,
+    theme: "light",
+    toggleVisible: () => {
+      null;
+    },
+  },
+} satisfies Story;
+
+const Dark = {
+  args: {
+    value: "password1234",
+    visible: true,
+    theme: "dark",
+    toggleVisible: () => {
+      null;
+    },
+  },
+} satisfies Story;
+
+const Focused = {
+  args: {
+    value: "password1234",
+    visible: true,
+    focused: true,
+    theme: "light",
+    toggleVisible: () => {
+      null;
+    },
+  },
+} satisfies Story;
+
+const Unfocused = {
+  args: {
+    value: "password1234",
+    visible: true,
+    focused: false,
+    theme: "light",
+    toggleVisible: () => {
+      null;
+    },
+  },
+} satisfies Story;
+
+const Stateful = ({ theme, placeholder }: Args) => {
+  const [value, setValue] = useState<string>("");
+  const [visible, setVisible] = useState<boolean>(true);
+  const [editable, setEditable] = useState<boolean>(false);
+  const toggleVisible = () => setVisible((state) => !state);
+  const toggleEditable = () => setEditable((state) => !state);
+  return (
+    <Hideable
+      theme={theme}
+      value={value}
+      visible={visible}
+      placeholder={placeholder}
+      focused={editable}
+      onValueChange={setValue}
+      toggleVisible={toggleVisible}
+      toggleFocused={toggleEditable}
+    />
+  );
+};
+
+const Dev = {
+  args: {
+    theme: "light",
+    placeholder: "Password",
+  } satisfies Partial<Args> as Args,
+  render: (args) => <Stateful {...args} />,
+} satisfies Story;
+
+export {
+  Visible,
+  Hidden,
+  WithVisibilitySwitch,
+  WithoutVisibilitySwitch,
+  Light,
+  Dark,
+  Focused,
+  Unfocused,
+  Dev,
+};
