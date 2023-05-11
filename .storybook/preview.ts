fa22b61db173720add991837ab4f535469e50b7b
tigeet <tigeeet@gmail.com>
2023-05-11 20:27:06 +0300

    Initial project setup

---
+++ .storybook/preview.ts
@@ -0,0 +1,17 @@
+import type { Preview } from "@storybook/react";
+import "@/reset.scss";
+import "@/global.scss";
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+};
+
+export default preview;
