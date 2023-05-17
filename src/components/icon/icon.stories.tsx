91a1e8af86d0df6f9a6b0ab42b5bc0a2e402fe14
tigeet <tigeeet@gmail.com>
2023-05-17 18:47:08 +0300

    feat: add theme, shape and size to `icon`

--- src/components/icon/icon.stories.tsx
+++ src/components/icon/icon.stories.tsx
@@ -2,11 +2,19 @@ import { Meta, StoryObj } from "@storybook/react";
 import React from "react";
 import Icon from "./icon";
 import { X } from "react-feather";
-
+import icon1 from "@assets/2af7b703ceb162244f5c1fb83b722db4.jpg";
+import icon2 from "@assets/4b53be184a0a7f2f80fcf63d0b58a365.jpg";
+import icon3 from "@assets/eye.svg";
+import { withSelect } from "@/storybookUtils/withSelect";
 const meta = {
   title: "Icon",
   component: Icon,
-  argTypes: {},
+  argTypes: {
+    size: withSelect(["small", "medium", "large"]),
+    shape: withSelect(["square", "circle", "rounded"]),
+    Content: withSelect([icon1, icon2, icon3]),
+    theme: withSelect(["light", "dark"]),
+  },
 } satisfies Meta<typeof Icon>;
 
 export default meta;
@@ -14,8 +22,9 @@ type Story = StoryObj<typeof meta>;
 
 const Primary: Story = {
   args: {
-    size: 16,
-    Content: X,
+    size: "medium",
+    Content: icon1,
+    // Content: icon1,
   },
 };
 
