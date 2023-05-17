f3044954b2e4e87a2fe09158a7ca9bef11731c45
tigeet <tigeeet@gmail.com>
2023-05-17 18:35:11 +0300

    ref: removed boolean utils

--- src/utils/boolean.ts
+++
@@ -1,7 +0,0 @@
-type Operand = boolean | undefined | null;
-
-const bool = (a: Operand): boolean => !!a;
-const implies = (a: Operand, b: Operand): boolean => {
-  return a ? bool(b) : true;
-};
-export { bool, implies };
